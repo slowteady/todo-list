c9dc74395cd51b8bb2a354fd43c8babeb47a7622
slowteady <vbn0213@gmail.com>
2023-03-14 20:54:44 +0900

    TodoList 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const GlobalStyle = createGlobalStyle`
@@ -14,7 +15,8 @@ const App = () => {
     <>
       <GlobalStyle />    
       <TodoTemplate>
-        <TodoHead />        
+        <TodoHead />      
+        <TodoList />  
       </TodoTemplate>
     </>
   )
