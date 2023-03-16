0a41d1ef6ffe8494d90368c0beab3be28332cc9f
slowteady <vbn0213@gmail.com>
2023-03-16 21:07:03 +0900

    TodoProvier로 감싸져 있지 않으면 오류 호출하도록 수정

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -58,13 +58,26 @@ export const TodoProvider = ({ children }) => {
 };
 
 export const useTodoState = () => {
-  return useContext(TodoStateContext);  
+  const context = useContext(TodoStateContext);  
+  if(!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+
 };
 
 export const useTodoDispatch = () => {
-  return useContext(TodoDispatchContext);
+  const context = useContext(TodoDispatchContext);
+  if(!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
 }
 
 export const useTodoNextId = () => {
-  return useContext(TodoNextIdContext);
+  const context = useContext(TodoNextIdContext);
+  if(!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
 }
