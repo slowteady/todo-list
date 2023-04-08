459e5bf19ca485853c998d6ac3bb817ab4566d79
slowteady <vbn0213@gmail.com>
2023-04-08 15:10:42 +0900

    Template 컴포넌트 중앙 위치 하도록 조정, background 이미지 추가하여 컴포넌트 구성

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import blackboard from "../img/blackboard.png";
+
+const TemplateBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Components = styled.div`
+  background: #efef56;
+  position: absolute;
   display: flex;
   flex-direction: column;
-  margin: 0 auto; 
-  background: yellow;
+  width: 600px;
+  height: 400px;
+  // border-radius: 16px;
+  // box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
 `;
 
 const Template = ({ children }) => {
   return (
     <>
-      <Components>{children}</Components>
+      <TemplateBox>
+        <Image src={blackboard} alt="Blackboard" />
+        <Components>{children}</Components>
+      </TemplateBox>
     </>
-  )
-}
+  );
+};
 
 export default Template;
