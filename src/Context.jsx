bfd59322120affc2ed100fea11239e638855cda6
slowteady <vbn0213@gmail.com>
2023-04-09 21:48:41 +0900

    초기 todo 리스트 빈 값으로 수정

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,28 +1,5 @@
 import React, { createContext, useContext, useReducer, useRef } from "react";
 
-const initialList = [
-  {
-    id: 1,
-    text: "테스트 아이템1",
-    done: true,
-  },
-  {
-    id: 2,
-    text: "테스트 아이템2",
-    done: true,
-  },
-  {
-    id: 3,
-    text: "테스트 아이템3",
-    done: false,
-  },
-  {
-    id: 4,
-    text: "테스트 아이템4",
-    done: false,
-  },
-];
-
 const reducer = (state, action) => {
   switch (action.type) {
     case "CREATE":
@@ -67,7 +44,7 @@ export const useTodoNextId = () => {
 };
 
 export const TodoProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialList);
+  const [state, dispatch] = useReducer(reducer, []);
   const nextId = useRef(5);
 
   return (
