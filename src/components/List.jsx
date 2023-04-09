abaa5f0854c69a502b5d611dba1d05c73ebf044c
slowteady <vbn0213@gmail.com>
2023-04-09 14:41:06 +0900

    List css 수정, Items 샘플 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import Items from "./Items";
 
 const ListBlock = styled.div`
   flex: 1;
-  padding: 28px 22px 14px;
+  padding: 18px 22px 14px;
   overflow-y: auto;
 `;
 
 const List = () => {
-  return <ListBlock></ListBlock>;
+  return (
+    <ListBlock>
+      <Items text="테스트 아이템" done={true}></Items>
+      <Items text="테스트 아이템2" done={true}></Items>
+      <Items text="테스트 아이템3" done={false}></Items>
+      <Items text="테스트 아이템4" done={false}></Items>
+    </ListBlock>
+  );
 };
 
 export default List;
