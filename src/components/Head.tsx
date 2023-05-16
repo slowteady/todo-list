f697ee08cf7e8464dc1938088184bf0cdeb626ad
slowteady <vbn0213@gmail.com>
2023-05-16 22:00:48 +0900

    head DOM 구현

---
+++ src/components/Head.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeadBox = styled.div`
+  padding: 28px 22px 14px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgb(90 152 93);
+  h1 {
+    margin: 0;
+    font-size: 28px;
+    color: rgb(31 32 33);
+  }
+  .day {
+    font-size: 17px;
+    margin-top: 5px;
+  }
+  .tasks-left {
+    color: rgb(255 115 14);
+    font-size: 15px;
+    font-weight: bold;
+    margin-top: 5px;
+  }
+`;
+
+ // 날짜 
+ const today = new Date();
+ const dateString = today.toLocaleDateString("ko-KR", {
+   year: "numeric",
+   month: "long",
+   day: "numeric",
+ });
+ const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
+
+const Head = () => {
+  return (
+    <HeadBox>
+      <div className="datebox">
+        <h1>{dateString}</h1>
+        <div className="day">{dayName}</div>
+      </div>
+      <div className="tasks-left">[leftState] LEFT</div>
+    </HeadBox>
+  );
+};
+
+export default Head;
