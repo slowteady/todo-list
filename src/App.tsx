379bffbeac8d3661c8a0b2e9c0d50d67bd3eede5
slowteady <vbn0213@gmail.com>
2023-05-16 22:02:43 +0900

    구현한 컴포넌트들 App.tsx에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import Template from "./components/Template";
+import Head from "./components/Head";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: rgb(255, 255, 255);
+  }
+`;
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Template>
+        <Head />
+      </Template>
+    </>
+  );
 };
 
 export default App;
