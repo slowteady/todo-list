011a108c9ca7892616d11d00d39db9e4520d350d
slowteady <vbn0213@gmail.com>
2023-05-17 22:16:36 +0900

    interface 사용하여 ReactNode 타입 할당 처리

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import blackboard from "../img/blackboard.png";
 
